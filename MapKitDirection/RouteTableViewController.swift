8dab9deb656e87fd711d51f1d8721f7bb70c24c6
Santhosh Chandrasekharan <csanthosh13@gmail.com>
2019-03-04 23:27:19 +0530

    Initial Commit

---
+++ MapKitDirection/RouteTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  RouteTableViewController.swift
+//  MapKitDirection
+
+
+import UIKit
+import MapKit
+
+class RouteTableViewController: UITableViewController {
+
+    var routeSteps = [MKRouteStep]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows
+        return routeSteps.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = routeSteps[indexPath.row].instructions
+        
+        return cell
+    }
+
+}
